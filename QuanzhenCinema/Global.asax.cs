292bcd2b81a9d22757cdc269202cb54b598314ea
Zili <450159653@qq.com>
2016-07-18 01:17:01 +0800

    Revert "权限测试"
    This reverts commit 6b74671fd040f77148c8bde0e73d10e8099dac9f.

--- QuanzhenCinema/Global.asax.cs
+++ QuanzhenCinema/Global.asax.cs
@@ -19,20 +19,3 @@ namespace QuanzhenCinema
         }
     }
 }
-
-
-void Application_AuthorizeRequest(object sender, System.EventArgs e)
-{
-    HttpApplication App = (HttpApplication)sender;
-    HttpContext Ctx = App.Context; //获取本次Http请求相关的HttpContext对象        
-    if (Ctx.Request.IsAuthenticated == true) //验证过的用户才进行role的处理  
-    {
-        FormsIdentity Id = (FormsIdentity)Ctx.User.Identity;
-        FormsAuthenticationTicket Ticket = Id.Ticket; //取得身份验证票         
-        string[] Roles = Ticket.UserData.Split(','); //将身份验证票中的role数据转成字符串数组      
-        Ctx.User = new System.Security.Principal.GenericPrincipal(Id, Roles); //将原有的Identity加上角色信息新建一个GenericPrincipal表示当前用户,这样当前用户就拥有了role信息       
-    }
-}
-如上所述，登录及验证匿名用户部分都没有问题，也能正常写入Cookies，登录之后通过
-FormsIdentity Id = (FormsIdentity)User.Identity;   
-FormsAuthenticationTicket Ticket = Id.Ticket; //取得身份验证票
