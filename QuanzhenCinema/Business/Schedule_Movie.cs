e317855ccc32071b501f188c9c13eed8a1db80ce
张锋 <377246620@qq.com>
2016-07-19 22:02:20 +0800

    增加排片前后端

--- QuanzhenCinema/Business/Schedule_Movie.cs
+++ QuanzhenCinema/Business/Schedule_Movie.cs
@@ -5,8 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using QuanzhenCinema.Models;
 
-namespace QuanzhenCinema.Business
-{
+namespace QuanzhenCinema.Business{
     public class DisplayMovieName_ViewModel
     {
         public int DISPLAY_ID { get; set; }
@@ -17,6 +16,7 @@ namespace QuanzhenCinema.Business
         public int LOWEST_PRICE { get; set; }
         public int LENGTH { get; set; }
         public string Name { get; set; }
+        public DateTime Expire_date { get; set; }
     }
 
     public class SCHEDULE_ViewModel
@@ -28,6 +28,7 @@ namespace QuanzhenCinema.Business
         public int ORIGINAL_PRICE { get; set; }
         public DateTime? START_TIME { get; set; }
         public DateTime? END_TIME { get; set; }
+        public string Name { get; set; }
     }
 
     public class HALL_ViewModel
@@ -39,34 +40,40 @@ namespace QuanzhenCinema.Business
         public int IS_IMAX { get; set; }
     }
 
-    public class Schedule_Movie
-    {
+    public class Package{
+        public Package(Schedule_Movie sm){
+            display = sm.getDisplay();
+            schedule = sm.getSchedule();
+            hall = sm.getHall();
+        }
+        public List<DisplayMovieName_ViewModel> display;
+        public List<SCHEDULE_ViewModel> schedule;
+        public List<HALL_ViewModel> hall;
+    }
+
+    public class Schedule_Movie{
         Quanzhen db;
         DateTime dt;
 
-        public Schedule_Movie(DateTime d)
-        {
+        public Schedule_Movie(DateTime d){
             db = new Quanzhen();
             dt = d;
-            movie();
         }
-        public List<DisplayMovieName_ViewModel> movie()
-        {
-            String sql = "select display_id,movie_id,is_3d,is_imax,language,lowest_price,length,name from display natural join movie  where expire_date > to_date('" + dt.Year + "-" + dt.Month + "-" + dt.Day + "','YYYY-MM-DD')";
+
+        public List<DisplayMovieName_ViewModel> getDisplay(){
+            String sql = "select display_id,movie_id,is_3d,is_imax,language,lowest_price,length,name,expire_date from display natural join movie where expire_date > to_date('" + dt.Year + "-" + dt.Month + "-" + dt.Day + "','YYYY-MM-DD')";
             List<DisplayMovieName_ViewModel> display_moviename = db.Database.SqlQuery<DisplayMovieName_ViewModel>(sql).ToList();
             return display_moviename;
         }
 
-        public List<SCHEDULE_ViewModel> schedule()
-        {
+        public List<SCHEDULE_ViewModel> getSchedule(){
             DateTime temp = dt.AddDays(1);
             String sql = "select * from schedule where start_time > to_date('" + dt.Year + '-' + dt.Month + '-' + dt.Day + "','YYYY-MM-DD') and end_time < to_date('" + temp.Year + '-' + temp.Month + '-' + temp.Day + "','YYYY-MM-DD')";
             List<SCHEDULE_ViewModel> s = db.Database.SqlQuery<SCHEDULE_ViewModel>(sql).ToList();
             return s;
         }
 
-        public List<HALL_ViewModel> Hall()
-        {
+        public List<HALL_ViewModel> getHall(){
             String sql = "select * from hall";
             List<HALL_ViewModel> s = db.Database.SqlQuery<HALL_ViewModel>(sql).ToList();
             return s.ToList();
--- QuanzhenCinema/Controllers/Schedule_MovieController.cs
+++ QuanzhenCinema/Controllers/Schedule_MovieController.cs
@@ -3,37 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using QuanzhenCinema.ViewModel;
 using QuanzhenCinema.Business;
 using QuanzhenCinema.Models;
 using System.Web.Script.Serialization;
+
 
 namespace QuanzhenCinema.Controllers
 {
     public class Schedule_MovieController : Controller
     {
-        // GET: Schedule_Movie
         Quanzhen db = new Quanzhen();
 
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public String Info(int Year, int Month, int Day)
-        {
-            //db.Configuration.ProxyCreationEnabled = false;
+        // GET: Schedule_Movie
+        public ActionResult getInfo(int Year, int Month, int Day){
             DateTime d = new DateTime(Year, Month, Day);
 
             Business.Schedule_Movie t = new Business.Schedule_Movie(d);
-            ClassPackage cp = new ClassPackage(t);
-            ViewBag.cp = cp;
+            Business.Package pkg = new Business.Package(t);
+            ViewBag.pkg = pkg;
+
+            var json = new JavaScriptSerializer().Serialize(ViewBag.pkg);
+            ViewBag.json = json;
+            return View(ViewBag);
+        }
+
+        public ActionResult AddUpdate()
+        {
+            //   ViewBag.DISPLAY_ID = new SelectList(db.DISPLAY, "DISPLAY_ID", "DISPLAY_ID");
+            //   ViewBag.HALL_ID = new SelectList(db.HALL, "HALL_ID", "HALL_ID");
+            return View();
+        }
 
 
-            //Object ret = ViewBag;
-            //Object ret = t.movie();
-            var json = new JavaScriptSerializer().Serialize(ViewBag.cp);
+        [HttpPost]
+        public ActionResult AddUpdate([Bind(Include = "SCHEDULE_ID,DISPLAY_ID,START_TIME,END_TIME,HALL_ID,ORIGINAL_PRICE,START_SLOT,END_SLOT")] SCHEDULE sCHEDULE)
+        {
+    //        if (ModelState.IsValid)
+     //       {
+                SCHEDULE s = db.SCHEDULE.Find(sCHEDULE.SCHEDULE_ID);
+                if (s == null)
+                {
+                sCHEDULE.SCHEDULE_ID = 0;
+                    db.SCHEDULE.Add(sCHEDULE);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    db.SCHEDULE.Remove(s);
+                    db.SaveChanges();
+                    db.SCHEDULE.Add(sCHEDULE);
+                    db.SaveChanges();
+                }
+     //       }
 
-            return json;
+            //   ViewBag.IsSussessful=
+            return View(sCHEDULE);
         }
     }
+
 }
