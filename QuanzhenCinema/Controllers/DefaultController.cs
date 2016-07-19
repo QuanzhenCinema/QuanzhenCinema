b8b1de630c89145bca86e6ac4f0b4d44eb65f592
fanhuiyang <1094803373@qq.com>
2016-07-19 20:57:51 +0800

    razor

---
+++ QuanzhenCinema/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanzhenCinema.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult developer()
+        {
+            return View();
+        }
+        public ActionResult description()
+        {
+            return View();
+        }
+        public ActionResult login()
+        {
+            return View();
+        }
+    }
+}
